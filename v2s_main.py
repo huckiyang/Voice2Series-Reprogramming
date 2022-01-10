8638502c7422fc80ec840e11d51d22fdb8749c81
huckiyang <huckiyang@gmail.com>
2022-01-10 18:17:29 -0500

    Update v2s_main.py

--- v2s_main.py
+++ v2s_main.py
@@ -27,7 +27,7 @@ args = parser.parse_args()
 
 x_train, y_train, x_test, y_test = readucr(args.dataset)
 
-if args.dataset == 2 # fix the public code error
+if args.dataset == 2: # fix the public code error
     y_train[y_train == -1] = 0
     y_test[y_test == -1] = 0 
 
